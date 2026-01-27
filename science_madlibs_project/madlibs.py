d815fa0220a3476b0a1ce1d23612be6d24c663b7
Vibha Rao <b.vibha.rao@gmail.com>
2026-01-26 19:20:34 -0800

    Wrote the abstract of the journal

---
+++ science_madlibs_project/madlibs.py
@@ -0,0 +1,39 @@
+#Introduction
+
+
+#Gather info
+
+#Generate Title
+
+#Authors
+print()
+author = input("What's your name? ")
+
+#Abstract(summary of the study)
+print(f"Ah, {author}, the renowed scientist! The scientific community will be ecstatic to hear about your findings! But before your scientific journal is released, I want to ask you some questions directly.")
+
+noun1 = input("Enter a noun: ")
+noun2 = input("Enter a plural noun: ")
+location = input("Enter a location: ")
+adverb1 = input("Enter an adverb: ")
+tool = input("Enter a tool: ")
+num = int(input("Enter a number(ex 1, 2, 3, 4): "))
+pastverb = input("Enter the past tense of a verb(ex hid, disappeared, ran, exploded):")
+
+#Intro: Why the study is important
+
+#Methods: We used blank to measure blank
+
+#Results: Percentage of blank showed behavior/trait
+
+#References: Citations to other papers
+
+
+#Print Abstract
+print("ABSTRACT:")
+print("_" *10)
+print(f"This study shows the effect of a {noun1} on a group of {noun2} in {location}.")
+print(f"We measured {num} {noun2} using a {tool}. Our results showed that the {noun2}")
+print(f"{adverb1} {pastverb} whenever confronted with a {noun1}")
+
+
